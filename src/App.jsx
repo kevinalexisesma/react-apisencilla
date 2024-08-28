278738fd60f4080d8aac6a76e2602169c3e2961b
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2024-08-28 08:57:06 -0500

    Feat(apiSencilla): Show users from apisencilla

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,18 @@ import { useFetch } from "./useFetch";
 import { fetchData } from "./fetchData";
 import "./App.css";
 
-// useFetch hook version
-// function App() {
-//   const { data, loading, error, handleCancelRequest } = useFetch(
-//     "https://jsonplaceholder.typicode.com/todos"
-//   );
-
-//   return (
-//     <div className="App">
-//       <h1>Fetch like a Pro</h1>
-
-//       <button onClick={handleCancelRequest}>Cancel Request</button>
-//       <ul className="card">
-//         {error && <li>Error: {error}</li>}
-//         {loading && <li>Loading...</li>}
-//         {data?.map((item) => (
-//           <li key={item.id}>{item.title}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-// --------------------------------------------
-// Pro Version - Render as you fetch
-const apiData = fetchData("https://jsonplaceholder.typicode.com/todos");
+const apiData = fetchData("http://localhost:8080/app/show");
 
 function App() {
   const data = apiData.read();
 
   return (
     <div className="App">
-      <h1>Fetch like a Pro</h1>
+      <h1>Users from Apisencilla</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <ul className="card">
           {data?.map((item) => (
-            <li key={item.id}>{item.title}</li>
+            <li key={item.id}>{item.name}</li>
           ))}
         </ul>
       </Suspense>
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,43 +1,6 @@
 import { useState, useEffect } from "react";
 
 export function useFetch(url) {
-  // Basic
-  //   const [data, setData] = useState(url);
-  //   useEffect(() => {
-  //     fetch(url)
-  //       .then((response) => response.json())
-  //       .then((json) => setData(json));
-  //   }, []);
-  //   return { data };
-
-  // ---------------------------------------------
-  //  With Loading
-  //   const [data, setData] = useState(null);
-  //   const [loading, setLoading] = useState(true);
-  //   useEffect(() => {
-  //     fetch(url)
-  //       .then((response) => response.json())
-  //       .then((json) => setData(json))
-  //       .finally(() => setLoading(false));
-  //   }, []);
-  //   return { data, loading };
-
-  // ---------------------------------------------
-  // With Error
-  //   const [data, setData] = useState(null);
-  //   const [loading, setLoading] = useState(true);
-  //   const [error, setError] = useState(null);
-  //   useEffect(() => {
-  //     fetch(url)
-  //       .then((response) => response.json())
-  //       .then((json) => setData(json))
-  //       .catch((error) => setError(error))
-  //       .finally(() => setLoading(false));
-  //   }, []);
-  //   return { data, loading, error };
-
-  // ---------------------------------------------
-  // With Abort Controller
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
